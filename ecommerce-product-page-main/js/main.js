d2a177f8a7c7535cc3ddc624aa1fa48e70ef888f
Bryce Woodland <bwoodland@vivacitytech.com>
2025-09-15 15:00:09 -0600

    Modal complete

--- ecommerce-product-page-main/js/main.js
+++ ecommerce-product-page-main/js/main.js
@@ -1,14 +1,41 @@
 const modal = document.querySelector('#imageModal');
-
-const img = document.querySelector('#thumbnail');
 const modalImg = document.querySelector('#img01');
-img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-}
+const closeBtn = document.querySelector('.close');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
 
-const span = document.querySelector('.close')[0];
+const thumbnails = document.querySelectorAll('.thumbnail');
+let currentIndex = 0;
 
-span.onclick = function() {
+// Open modal when thumbnail clicked
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener('click', function () {
+    modal.style.display = "flex";
+    currentIndex = index;
+    modalImg.src = this.dataset.full;
+  });
+});
+
+// Show previous image
+prevBtn.onclick = function () {
+  currentIndex = (currentIndex - 1 + thumbnails.length) % thumbnails.length;
+  modalImg.src = thumbnails[currentIndex].dataset.full;
+};
+
+// Show next image
+nextBtn.onclick = function () {
+  currentIndex = (currentIndex + 1) % thumbnails.length;
+  modalImg.src = thumbnails[currentIndex].dataset.full;
+};
+
+// Close modal
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Optional: close when clicking outside the image
+modal.onclick = function (e) {
+  if (e.target === modal) {
     modal.style.display = "none";
-}
+  }
+};
